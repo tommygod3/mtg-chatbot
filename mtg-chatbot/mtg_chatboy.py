d670c62ef2b062f3969c8fc9fd1941ec7ffbf486
tommygod3 <tommygod1@live.com>
2020-02-24 23:44:58 +0000

    Added NLTK basic ownership system

--- mtg-chatbot/mtg_chatboy.py
+++ mtg-chatbot/mtg_chatboy.py
@@ -9,17 +9,39 @@ import tensorflow as tf
 from tensorflow.keras import models, backend, layers
 import urllib.request
 import numpy
+import nltk
+import pickle
 
 import scrython
 from scrython.foundation import ScryfallError
 
 class Chatbot:
-    def __init__(self, aiml_filename, model):
+    def __init__(self, aiml_filename, model, grammar_file):
         self.aiml_filename = Chatbot.get_absolute_path(aiml_filename)
         self.set_kernel()
         self.set_patterns()
         self.load_image_models(model)
         self.set_classnames()
+        self.load_nltk(grammar_file)
+
+    def load_nltk(self, grammar_file):
+        valuation_string = """
+        card => {}
+        my_hand => h1
+        opp_hand => h2
+        my_graveyard => g1
+        opp_graveyard => g2
+        my_exile => e1
+        opp_exile => e2
+        my_battlefield => b1
+        opp_battlefield => b2
+        be_in => {}
+        """
+        self.valuation = nltk.Valuation.fromstring(valuation_string)
+        #self.valuation = pickle.load(open("save.p", "rb"))
+        #pickle.dump(self.valuation, open("save.p", "wb"))
+        self.grammar_file = Chatbot.get_absolute_path(grammar_file)
+        self.object_counter = 0
 
     def get_absolute_path(filename):
         return f"{os.path.dirname(os.path.realpath(sys.argv[0]))}{os.path.sep}{filename}"
@@ -27,10 +49,10 @@ class Chatbot:
     def is_command(self, user_input):
         return user_input[0] == "#"
 
-    def get_command_and_parameter(self, user_input):
+    def get_command_and_parameters(self, user_input):
         command = user_input[1:].split("$")[0]
-        parameter = user_input[1:].split("$")[1]
-        return command, parameter
+        parameters = user_input[1:].split("$")[1:]
+        return command, parameters
 
     def set_kernel(self):
         self.kernel = aiml.Kernel()
@@ -260,39 +282,189 @@ class Chatbot:
             most_similar_index = similarities.index(max(similarities))
             self.handle_answer(self.kernel.respond(self.patterns[most_similar_index]))
 
+    def translate_ownership(self, input):
+        vocabulary = {
+            "i": "my",
+            "opponent": "opp",
+            "my": "my"
+        }
+        for term, ownership in vocabulary.items():
+            if term in input:
+                return ownership
+
+    def translate_zone(self, input):
+        vocabulary = {
+            "hand": "hand",
+            "graveyard": "graveyard",
+            "exile": "exile",
+            "battlefield": "battlefield"
+        }
+        for term, zone in vocabulary.items():
+            if term in input:
+                return zone
+    
+    def encode_card_name(self, card_name):
+        self.object_counter += 1
+        card = scrython.cards.Named(exact=card_name)
+        return f"{card.name()}${str(self.object_counter)}"
+
+    def decode_card_name(self, object_name):
+        return object_name.split("$")[0]
+
+    def is_a_permanent(self, card_name):
+        card = scrython.cards.Named(exact=card_name)
+        if "Sorcery" in card.type_line() or "Instant" in card.type_line():
+            return False
+        return True
+    
+    def add_to_zone(self, player, zone, card_name):
+        ownership = self.translate_ownership(player)
+        location = f"{ownership}_{zone}"
+        try:
+            object_id = self.encode_card_name(card_name)
+            # insert constant
+            self.valuation[object_id] = object_id 
+            # clean up default model value
+            if len(self.valuation["card"]) == 1:
+                if ('',) in self.valuation["card"]:
+                    self.valuation["card"].clear()
+
+            # insert object_id into card list
+            self.valuation["card"].add((object_id,))
+
+            # clean up default model value
+            if len(self.valuation["be_in"]) == 1:
+                if ('',) in self.valuation["be_in"]:
+                    self.valuation["be_in"].clear()
+            # insert object_id and zone into ownership
+            self.valuation["be_in"].add((object_id, self.valuation[location]))
+        except ScryfallError as e:
+            print(e)
+    
+    def draw_card(self, player, card_name):
+        self.add_to_zone(player, "hand", card_name)
+
+    def cast_card(self, player, card_name):
+        remove_from_hand_result = self.remove(card_name, f"{player} hand")
+        if remove_from_hand_result:
+            print(f"{remove_from_hand_result} in hand")
+            return
+
+        if self.is_a_permanent(card_name):
+            self.add_to_zone(player, "battlefield", card_name)
+        else:
+            self.add_to_zone(player, "graveyard", card_name)
+    
+    def is_card_in_zone(self, location_input):
+        g = nltk.Assignment(self.valuation.domain)
+        m = nltk.Model(self.valuation.domain, self.valuation)
+        ownership = self.translate_ownership(location_input)
+        zone = self.translate_zone(location_input)
+        location = f"{ownership}_{zone}"
+        sent = "some card are_in " + location
+        results = nltk.evaluate_sents([sent], self.grammar_file, m, g)[0][0]
+        if results[2] == True:
+            print("Yes.")
+        else:
+            print("No.")
+
+    def is_all_cards_in_zone(self, location_input):
+        g = nltk.Assignment(self.valuation.domain)
+        m = nltk.Model(self.valuation.domain, self.valuation)
+        ownership = self.translate_ownership(location_input)
+        zone = self.translate_zone(location_input)
+        location = f"{ownership}_{zone}"
+        sent = "all card are_in " + location
+        results = nltk.evaluate_sents([sent], self.grammar_file, m, g)[0][0]
+        if results[2] == True:
+            print("Yes.")
+        else:
+            print("No.")
+
+    def get_cards_in_zone(self, location_input):
+        g = nltk.Assignment(self.valuation.domain)
+        m = nltk.Model(self.valuation.domain, self.valuation)
+        ownership = self.translate_ownership(location_input)
+        zone = self.translate_zone(location_input)
+        location = f"{ownership}_{zone}"
+        e = nltk.Expression.fromstring("be_in(x," + location + ")")
+        sat = m.satisfiers(e, "x", g)
+        if len(sat) == 0:
+            print("None.")
+        else:
+            # find satisfying objects in the valuation dictionary,
+            # and print their decoded object_id names
+            sol = self.valuation.values()
+            for so in sat:
+                print(self.decode_card_name(so))
+    
+    def remove(self, card_name, location_input):
+        g = nltk.Assignment(self.valuation.domain)
+        m = nltk.Model(self.valuation.domain, self.valuation)
+        ownership = self.translate_ownership(location_input)
+        zone = self.translate_zone(location_input)
+        location = f"{ownership}_{zone}"
+        e = nltk.Expression.fromstring("be_in(x," + location + ")")
+        to_remove = m.satisfiers(e, "x", g)
+        if len(to_remove) == 0:
+            return f"No cards named {card_name}"
+        else:
+            for relationship in self.valuation["be_in"]:
+                if relationship[0] in to_remove:
+                    self.valuation["be_in"].remove(relationship)
+                    return False
+
     def handle_answer(self, answer):
         if not answer:
             print("I don't understand :(")
         if self.is_command(answer):
-            command, parameter = self.get_command_and_parameter(answer)
+            command, parameters = self.get_command_and_parameters(answer)
             if command == "quit":
-                print(parameter)
+                print(parameters[0])
                 return True
 
             if command == "image_classification":
-                self.print_image_model_result(parameter)
+                self.print_image_model_result(parameters[0])
             if command == "describe":
-                self.print_description(parameter)
+                self.print_description(parameters[0])
             if command == "describe_random":
                 self.print_description_random()
             if command == "colour":
-                self.print_colour(parameter)
+                self.print_colour(parameters[0])
             if command == "cost":
-                self.print_cost(parameter)
+                self.print_cost(parameters[0])
             if command == "type":
-                self.print_type(parameter)
+                self.print_type(parameters[0])
             if command == "text":
-                self.print_text(parameter)
+                self.print_text(parameters[0])
             if command == "price":
-                self.print_price(parameter)
+                self.print_price(parameters[0])
             if command == "favourite":
-                self.print_favourite(parameter)
+                self.print_favourite(parameters[0])
             if command == "show":
-                self.show_card(parameter)
+                self.show_card(parameters[0])
             if command == "show_random":
                 self.show_card_random()
+            if command == "draw":
+                self.draw_card(parameters[0], parameters[1])
+            if command == "cast":
+                self.cast_card(parameters[0], parameters[1])
+            if command == "cards_in_zone":
+                self.is_card_in_zone(parameters[0])
+            if command == "all_cards_in":
+                self.is_all_cards_in_zone(parameters[0])
+            if command == "which_cards_in":
+                self.get_cards_in_zone(parameters[0])
+            if command == "remove":
+                self.remove(parameters[0], parameters[1])
+
+
+
+
+
+
             if command == "default":
-                self.similarity(parameter)
+                self.similarity(parameters[0])
         else:
             print(answer)
 
@@ -320,4 +492,5 @@ class Chatbot:
 
 
 Chatbot(aiml_filename="aiml-mtg.xml",
-        model="../../mtg-image-classify/classify/model.h5").run()
+        model="../../mtg-image-classify/classify/model.h5",
+        grammar_file="simple-sem.fcfg").run()
